95e8961269bece69772ca566a410c1639c6c001b
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-03-18 13:14:04 +0100

    push changes on withdraw

--- app/Http/Controllers/Api/UltilityController.php
+++ app/Http/Controllers/Api/UltilityController.php
@@ -223,8 +223,24 @@ class UltilityController extends Controller
     }
 
     public function withdraw(Request $request){
-
         $input=$request->all();
+        $u=User::where("user_name", $input['user_name'])->first();
+
+
+        if($input['wallet']=="Mega Bonus"){
+            if($u->bonus < $input['amount']){
+                return response()->json(['success'=> 0, 'message'=>'Low wallet balance']);
+            }
+        }
+
+        if($input['wallet']=="Agent Commission"){
+            if($u->agent_commision < $input['amount']){
+                return response()->json(['success'=> 0, 'message'=>'Low wallet balance']);
+            }
+        }
+
+        $u->agent_commision -= $input['amount'];
+        $u->save();
 
         Withdraw::create($input);
 
