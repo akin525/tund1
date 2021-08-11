d2771102bc4f2322cfc510d9b7a9bb8ce3488647
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-08-11 23:58:34 +0100

    push changes

--- app/Http/Controllers/Api/SellDataController.php
+++ app/Http/Controllers/Api/SellDataController.php
@@ -24,31 +24,29 @@ class SellDataController extends Controller
 
         $rac = ResellerDataPlans::where("code", strtolower($input['coded']))->first();
 
-        $curl = curl_init();
+//        $curl = curl_init();
+//
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => env('SERVER1N') . "data" . env('SERVER1N_AUTH') . "&network=" . $netcode . "&phoneNumber=" . $phone . "&product_code=" . $code . "&price=" . $rac->price . "&trans_id=" . $transid . "&return_url=https://mcd.com",
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => '',
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 0,
+//            CURLOPT_FOLLOWLOCATION => true,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => 'GET',
+//            CURLOPT_HTTPHEADER => array(
+//                'Content-Type: application/json'
+//            ),
+//        ));
+//        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+//
+//        $response = curl_exec($curl);
+//
+//        curl_close($curl);
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => env('SERVER1N') . "data" . env('SERVER1N_AUTH') . "&network=" . $netcode . "&phoneNumber=" . $phone . "&product_code=" . $code . "&price=" . $rac->price . "&trans_id=" . $transid . "&return_url=https://mcd.com",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json'
-            ),
-        ));
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-//        echo $response;
-//        return "samji";
-
-//        $response='{"trans_id":"R16287146091881817015","details":{"network":"MTN","data_volume":"1GB","phone_number":"08166939205","price":"255","status":"Pending","balance":"15805"}}';
+        $response = '{"trans_id":"R16287146091881817015","details":{"network":"MTN","data_volume":"1GB","phone_number":"08166939205","price":"255","status":"Pending","balance":"15805"}}';
 
         $rep = json_decode($response, true);
 
@@ -73,33 +71,30 @@ class SellDataController extends Controller
     public function server6($request, $code, $phone, $transid, $net, $input, $dada, $requester)
     {
 
-        $curl = curl_init();
+//        $curl = curl_init();
+//
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => env('SERVER6') . "pay",
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => '',
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 0,
+//            CURLOPT_FOLLOWLOCATION => true,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => 'POST',
+//            CURLOPT_POSTFIELDS => '{"request_id": "' . $transid . '", "serviceID": "' . $net . '","variation_code": "' . $code . '","phone": "' . $phone . '","billersCode": "' . $phone . '"}',
+//            CURLOPT_HTTPHEADER => array(
+//                'Authorization: Basic ' . env('SERVER6_AUTH'),
+//                'Content-Type: application/json'
+//            ),
+//        ));
+//        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+//
+//        $response = curl_exec($curl);
+//
+//        curl_close($curl);
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => env('SERVER6') . "pay",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => '{"request_id": "' . $transid . '", "serviceID": "' . $net . '","variation_code": "' . $code . '","phone": "' . $phone . '","billersCode": "' . $phone . '"}',
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Basic ' . env('SERVER6_AUTH'),
-                'Content-Type: application/json'
-            ),
-        ));
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-//        echo $response;
-//        return;
-
-//        $response='{"code":"000","content":{"transactions":{"status":"delivered","product_name":"MTNData","unique_element":"08166939205","unit_price":100,"quantity":1,"service_verification":null,"channel":"api","commission":3,"total_amount":97,"discount":null,"type":"DataServices","email":"odejinmisamuel@gmail.com","phone":"08166939205","name":null,"convinience_fee":0,"amount":100,"platform":"api","method":"api","transactionId":"16287015152955612203232964"}},"response_description":"TRANSACTIONSUCCESSFUL","requestId":"R16287015121692605289","amount":"100.00","transaction_date":{"date":"2021-08-1118:05:15.000000","timezone_type":3,"timezone":"Africa\/Lagos"},"purchased_code":""}';
+        $response = '{"code":"000","content":{"transactions":{"status":"delivered","product_name":"MTNData","unique_element":"08166939205","unit_price":100,"quantity":1,"service_verification":null,"channel":"api","commission":3,"total_amount":97,"discount":null,"type":"DataServices","email":"odejinmisamuel@gmail.com","phone":"08166939205","name":null,"convinience_fee":0,"amount":100,"platform":"api","method":"api","transactionId":"16287015152955612203232964"}},"response_description":"TRANSACTIONSUCCESSFUL","requestId":"R16287015121692605289","amount":"100.00","transaction_date":{"date":"2021-08-1118:05:15.000000","timezone_type":3,"timezone":"Africa\/Lagos"},"purchased_code":""}';
 
         $rep = json_decode($response, true);
 
--- app/Http/Controllers/Api/SellElectricityController.php
+++ app/Http/Controllers/Api/SellElectricityController.php
@@ -10,31 +10,31 @@ class SellElectricityController extends Controller
     public function server6($request, $code, $phone, $transid, $net, $input, $dada, $requester)
     {
 
-        $curl = curl_init();
+//        $curl = curl_init();
+//
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => env('SERVER6') . "pay",
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => '',
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 0,
+//            CURLOPT_FOLLOWLOCATION => true,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => 'POST',
+//            CURLOPT_POSTFIELDS => '{"request_id": "' . $transid . '", "serviceID": "' . $code . '","variation_code": "prepaid","phone": "' . $phone . '","billersCode": "' . $phone . '","amount": "' . $request->get('amount') . '"}',
+//            CURLOPT_HTTPHEADER => array(
+//                'Authorization: Basic ' . env('SERVER6_AUTH'),
+//                'Content-Type: application/json'
+//            ),
+//        ));
+//        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+//
+//        $response = curl_exec($curl);
+//
+//        curl_close($curl);
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => env('SERVER6') . "pay",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => '{"request_id": "' . $transid . '", "serviceID": "' . $code . '","variation_code": "prepaid","phone": "' . $phone . '","billersCode": "' . $phone . '","amount": "' . $request->get('amount') . '"}',
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Basic ' . env('SERVER6_AUTH'),
-                'Content-Type: application/json'
-            ),
-        ));
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-
-//        $response = '{ "code": "000", "content": { "transactions": { "amount": 1000, "convinience_fee": 0, "status": "delivered", "name": null, "phone": "07061933309", "email": "sandbox@vtpass.com", "type": "Electricity Bill", "created_at": "2019-08-17 02:27:26", "discount": null, "giftcard_id": null, "total_amount": 992, "commission": 8, "channel": "api", "platform": "api", "service_verification": null, "quantity": 1, "unit_price": 1000, "unique_element": "1010101010101", "product_name": "Eko Electric Payment - EKEDC" } }, "response_description": "TRANSACTION SUCCESSFUL", "requestId": "hg3hgh3gdiud4w2wb33", "amount": "1000.00", "transaction_date": { "date": "2019-08-17 02:27:27.000000", "timezone_type": 3, "timezone": "Africa/Lagos" }, "purchased_code": "Token : 42167939781206619049 Bonus Token : 62881559799402440206", "mainToken": "42167939781206619049", "mainTokenDescription": "Normal Sale", "mainTokenUnits": 16666.666, "mainTokenTax": 442.11, "mainsTokenAmount": 3157.89, "bonusToken": "62881559799402440206", "bonusTokenDescription": "FBE Token", "bonusTokenUnits": 50, "bonusTokenTax": null, "bonusTokenAmount": null, "tariffIndex": "52", "debtDescription": "1122" }';
+        $response = '{ "code": "000", "content": { "transactions": { "amount": 1000, "convinience_fee": 0, "status": "delivered", "name": null, "phone": "07061933309", "email": "sandbox@vtpass.com", "type": "Electricity Bill", "created_at": "2019-08-17 02:27:26", "discount": null, "giftcard_id": null, "total_amount": 992, "commission": 8, "channel": "api", "platform": "api", "service_verification": null, "quantity": 1, "unit_price": 1000, "unique_element": "1010101010101", "product_name": "Eko Electric Payment - EKEDC" } }, "response_description": "TRANSACTION SUCCESSFUL", "requestId": "hg3hgh3gdiud4w2wb33", "amount": "1000.00", "transaction_date": { "date": "2019-08-17 02:27:27.000000", "timezone_type": 3, "timezone": "Africa/Lagos" }, "purchased_code": "Token : 42167939781206619049 Bonus Token : 62881559799402440206", "mainToken": "42167939781206619049", "mainTokenDescription": "Normal Sale", "mainTokenUnits": 16666.666, "mainTokenTax": 442.11, "mainsTokenAmount": 3157.89, "bonusToken": "62881559799402440206", "bonusTokenDescription": "FBE Token", "bonusTokenUnits": 50, "bonusTokenTax": null, "bonusTokenAmount": null, "tariffIndex": "52", "debtDescription": "1122" }';
 
         $rep = json_decode($response, true);
 
