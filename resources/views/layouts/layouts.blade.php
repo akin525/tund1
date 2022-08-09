a97347b199eb16fb0919f697246be7c92051b110
odejinmisa <odejinmisa@newwavesecosystem.com>
2022-08-09 14:59:08 +0100

    commit changes

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -274,7 +274,7 @@
                         </li>
                     </ul>
                     <ul class="list-inline menu-left mb-0">
-{{--                        <li class="float-left"><button class="button-menu-mobile open-left waves-light waves-effect"><i class="mdi mdi-menu"></i></button></li>--}}
+                        <li class="float-left"><button class="button-menu-mobile open-left waves-light waves-effect"><i class="mdi mdi-menu"></i></button></li>
 {{--                        <li class="hide-phone app-search">--}}
 {{--                            <form role="search" class=""><input type="text" placeholder="Search..." class="form-control"> <a href="#"><i class="fas fa-search"></i></a></form>--}}
 {{--                        </li>--}}
