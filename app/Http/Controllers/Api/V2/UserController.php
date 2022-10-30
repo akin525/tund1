59e94d5320bc750534264f4e2d0542af674be566
odejinmisa <odejinmisa@newwavesecosystem.com>
2022-10-30 19:51:32 +0100

    push changes

--- app/Http/Controllers/Api/V2/UserController.php
+++ app/Http/Controllers/Api/V2/UserController.php
@@ -77,7 +77,7 @@ class UserController extends Controller
         $others['support_whatsapp'] = $sett['support_whatsapp'];
         $others['biz_verification_price_customer'] = $sett['biz_verification_price_customer'];
         $others['reseller_terms'] = $sett['reseller_terms'];
-        $services['privacy_policy'] = $sett['privacy_policy'];
+        $others['privacy_policy'] = $sett['privacy_policy'];
 
         return response()->json(['success' => 1, 'message' => 'Fetched successfully', 'data' => ['user' => $me, 'balances' => $balances, 'services' => $services, 'news' => $user->gnews, 'others' => $others]]);
     }
